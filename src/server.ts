9bc34410a8e8c19b84b4096e5878e4f370eff000
Raphael Coelho <raphael_coelho@hotmail.com.br>
2022-09-05 07:34:32 -0300

    infra: add docker files and setup postgres connection

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,21 @@
 import express, { json } from "express"
 import { routes } from "./routes"
 
+// typeorm
+import "reflect-metadata"
+import './database'
+
+// swagger
 import swaggerUi from 'swagger-ui-express'
 import swaggerFile from './swagger.json'
 
+const port = 3333
 const app = express()
-app.use(json())
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+app.use(json())
 
 app.use(routes)
 
-const port = 3333
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
 app.listen(port, () => console.log(`Server is running on port: ${port} 🚀🚀🚀`))
