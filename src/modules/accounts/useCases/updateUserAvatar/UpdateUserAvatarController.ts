c009d1257eb977c19bebbc2dc7de73196a7c7a87
Raphael Coelho <raphael_coelho@hotmail.com.br>
2022-09-25 17:11:04 -0300

    feat: refactor uploadCarsImagesUseCase with new storage provider

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController.ts
@@ -1,17 +1,18 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
+
 import { UpdateUserAvatarUseCase } from "./UpdateUserAvatarUseCase";
 
 class UpdateUserAvatarController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { id } = request.user
-    const avatar_file = request.file.filename
+    const { id } = request.user;
+    const avatar_file = request.file.filename;
 
-    const updateUseravatarUseCase = container.resolve(UpdateUserAvatarUseCase)
-    await updateUseravatarUseCase.execute({ user_id: id, avatar_file })
+    const updateUseravatarUseCase = container.resolve(UpdateUserAvatarUseCase);
+    await updateUseravatarUseCase.execute({ user_id: id, avatar_file });
 
-    return response.status(204).send()
+    return response.status(204).send();
   }
 }
 
-export { UpdateUserAvatarController }
+export { UpdateUserAvatarController };
--- src/modules/cars/useCases/cars/uploadCarImages/UploadCarImagesUseCase.ts
+++ src/modules/cars/useCases/cars/uploadCarImages/UploadCarImagesUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 
+import { IStorageProvider } from "../../../../../shared/container/providers/StorageProvider/IStorageProvider";
 import { ICarsImagesRepository } from "../../../repositories/ICarsImagesRepository";
 
 interface IRequest {
@@ -11,12 +12,15 @@ interface IRequest {
 class UploadCarImagesUseCase {
   constructor(
     @inject("CarsImagesRepository")
-    private carsImageRepository: ICarsImagesRepository
+    private carsImageRepository: ICarsImagesRepository,
+    @inject("StorageProvider")
+    private storageProvider: IStorageProvider
   ) {}
 
   async execute({ car_id, images_name }: IRequest): Promise<void> {
-    images_name.map(async (image_name) => {
-      await this.carsImageRepository.create(car_id, image_name);
+    images_name.map(async (image) => {
+      await this.carsImageRepository.create(car_id, image);
+      await this.storageProvider.save(image, "cars");
     });
   }
 }
